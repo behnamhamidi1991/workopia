54e27156f55f187759b531f1c03273a59f9294ab
Behnam Hamidi <eslp91@hotmail.com>
2024-03-18 18:35:00 +0330

    partials route

--- helpers.php
+++ helpers.php
@@ -10,3 +10,27 @@
  function basePath($path = '') {
     return __DIR__ . '/' . $path;
  }
+
+ /**
+  * Load a view
+  *
+  * @param string $name
+  * @return void
+  *
+  */
+
+  function loadView ($name) {
+   require basePath("views/{$name}.view.php");
+  }
+
+
+  /**
+   * Load a partial
+   * 
+   * @param string $name
+   * @return void
+   * 
+   */
+   function loadPartial($name) {
+      require basePath("views/partials/{$name}.php");
+   }
