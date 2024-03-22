651225c208d88f1d8fa258245b2e09c17a00f5c3
Behnam Hamidi <eslp91@hotmail.com>
2024-03-22 17:46:18 +0330

    htaccess

--- public/index.php
+++ public/index.php
@@ -1,3 +1,13 @@
 <?php 
 require '../helpers.php';
-loadView('home');
+
+$routes = [
+    '/' => 'controllers/home.php', 
+    '/listings' => 'controllers/listings/index.php',
+    '/listings/create' => 'controllers/listings/create.php',
+    '404' => '/controllers/error/404.php'
+];
+
+$uri = $_SERVER['REQUEST_URI'];
+
+inspectAndDie($uri);
