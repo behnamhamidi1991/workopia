c1b0ccac2f6a89dfe0bd6b78c527f29a7c25bed6
Behnam Hamidi <eslp91@hotmail.com>
2024-03-22 15:01:18 +0330

    loadView

--- helpers.php
+++ helpers.php
@@ -9,3 +9,14 @@
  function basePath($path = '') {
     return __DIR__ . '/' . $path;
  }
+
+ /**
+  * Load a view
+  *
+  * @param string $name
+  * @return void
+  * 
+  */
+  function loadView($name) {
+    require basePath("views/{$name}.view.php");
+  }
