2b94a0a262f4ca92f4b725cfdbe408db7d847201
Behnam Hamidi <eslp91@hotmail.com>
2024-03-24 19:36:37 +0330

    All listings refactored

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -25,4 +25,18 @@ class ListingController {
     public function create() {
         loadView('listings/create');
     }
+
+    public function show() {
+        $id = $_GET['id'] ?? '';
+
+            $params = [
+                'id' => $id
+            ];
+            
+            $listing = $this->db->query("SELECT * FROM listings WHERE id = :id", $params)->fetch();
+
+            loadView('listings/show', [
+                'listing' => $listing
+            ]);
+        }
 }
