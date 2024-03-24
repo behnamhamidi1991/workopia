7b442dee7af746e0c46b42478b4b58f4829c1ce9
Behnam Hamidi <eslp91@hotmail.com>
2024-03-24 19:10:29 +0330

    Get application back to function

--- App/controllers/HomeController.php
+++ App/controllers/HomeController.php
@@ -5,6 +5,9 @@ namespace App\Controllers;
 class HomeController {
     public function __construct()
     {
-        die('HomeController');
+    }
+    
+    public function index() {
+        die('HomeController@index');
     }
 }
