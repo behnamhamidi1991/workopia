8ab366a663d3c16815b6ead23dd4f7068b5f3dd8
Behnam Hamidi <eslp91@hotmail.com>
2024-03-24 19:29:22 +0330

    createMethod

--- routes.php
+++ routes.php
@@ -1,6 +1,7 @@
 <?php 
 
 $router->get('/', 'HomeController@index');
+$router->get('/listings', 'ListingController@index');
 // $router->get('/', 'controllers/home.php');
 // $router->get('/listings', 'controllers/listings/index.php');
 // $router->get('/listings/create', 'controllers/listings/create.php');
