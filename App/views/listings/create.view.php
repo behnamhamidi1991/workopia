c1bb83b2252716973bb65e68a78b356a78adf979
Behnam Hamidi <eslp91@hotmail.com>
2024-03-25 17:21:31 +0330

    Commit

--- App/views/listings/create.view.php
+++ App/views/listings/create.view.php
@@ -37,6 +37,7 @@
               placeholder="Job Description"
               class="w-full px-4 py-2 border rounded focus:outline-none"
               <?= $listing['description'] ?? '' ?>
+
             ></textarea>
           </div>
           <div class="mb-4">
