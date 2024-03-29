72cf74c57e9abda60b56e362098608c00d8c553c
Behnam Hamidi <eslp91@hotmail.com>
2024-03-29 11:51:08 +0330

    go php

--- routes.php
+++ routes.php
@@ -5,3 +5,4 @@ $router->get('/listings', 'ListingController@index');
 $router->get('/listings/create', 'ListingController@create');
 $router->get('/listing', 'ListingController@show');
 $router->post('/listings', 'ListingController@store');
+$router->delete('/listings/{id}', 'listingController@destroy');
